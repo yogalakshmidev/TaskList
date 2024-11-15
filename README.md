659d4885cbd78bfe5584a3674efb6ba700880ca8
Yogalakshmi <yogarajakanibe@gmail.com>
2024-11-16 03:04:34 +0530

    New Files added for Expense Tracker

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-seperate repository for the task
+Expense Tracker design used to track the daily expenses.
+In this, we can also able to add, delete and edit the details basic design
--- index.js
+++ index.js
@@ -1,38 +1,111 @@
-console.log("Hello javascript");
-const promise= new Promise((resolve,reject)=>{
-  setTimeout(()=>{
-  // fetch data
-  // do something with the data
-  let data=10;
-  let successFactor=false;
-  // take decision whether the operation is success or not
-  if(successFactor){
-    resolve(data);
+const currencyHolder=document.getElementById("currency");
+const balanceHolder=document.getElementById("balance");
 
+const tnxNameHolder=document.getElementById("name");
+const tnxAmountHolder=document.getElementById("amount");
+
+const income=document.getElementById("income");
+const expense=document.getElementById("expense");
+
+const saveButton=document.getElementById("save");
+const cancelButton=document.getElementById("cancel");
+const displayList=document.getElementById("list_of_transactions");
+
+let symbol="$";
+let listOfTransactions = [];
+let currentBalance = 0;
+let editIndex = -1;
+
+function edit(i){
+  editIndex = i;
+tnxNameHolder.value = listOfTransactions[i].name;
+tnxAmountHolder.value = listOfTransactions[i].amount;
+if(listOfTransactions[i].type == "income"){
+  income.checked = true;
+}
+else{
+  expense.checked = true;
+}
+cancelButton.style.display="block";
+}
+function del(i){
+  listOfTransactions = listOfTransactions.filter((e,index) => i !== index);
+  render();
+}
+
+function saveData(){
+  localStorage.setItem("symbol",symbol);
+  localStorage.setItem("balance",currentBalance);
+  localStorage.setItem("list",JSON.stringify(listOfTransactions));
+}
+function render(){
+   currentBalance =  listOfTransactions.reduce((total,value)=>{
+    return value.type == "expense" ? total - value.amount : total + value.amount
+  }, 0)
+
+    displayList.innerHTML="";
+
+    if(listOfTransactions.length == 0){
+      displayList.innerHTML +="No Transactions found"
+    }
+
+    else{
+      listOfTransactions.forEach((e,i) =>{
+        displayList.innerHTML +=` 
+        <li class="transaction ${e.type}">
+          <p>${e.name}</p>
+          <div class="right_side">
+            <p>${symbol}${e.amount}</p>
+            <button onclick="edit(${i})"><i class="fa-regular fa-pen-to-square"></i></button>
+            <button onclick="del(${i})"><i class="fa-solid fa-trash"></i></button>
+          </div>
+        </li>
+        `;
+      })
+    }
+
+
+  currencyHolder.innerHTML = symbol;
+  balanceHolder.innerHTML = currentBalance;
+  saveData();
+
+}
+
+cancelButton.addEventListener("click",()=>{
+  editIndex=-1;
+  // listOfTransactions.push(transaction);
+  tnxNameHolder.value="";
+  tnxAmountHolder.value="";
+  cancelButton.style.display="none";
+})
+
+saveButton.addEventListener("click",()=>{
+  if(tnxNameHolder.value == "" || Number(tnxAmountHolder.value) <=0){
+    console.log("Please enter proper transaction");
+    return;
   }
-  else{
-    reject("Network error in the operation");
+  let transaction = {
+    name: tnxNameHolder.value,
+    amount:Number(tnxAmountHolder.value),
+    type:income.checked? "income" :"expense"
+
+  };
+  console.log(transaction);
+if(editIndex == -1){ 
+  listOfTransactions.push(transaction);
+}
+else{
+  listOfTransactions[editIndex]=transaction;
   }
-    },3000);
-});
 
-promise
-  .then((data)=>{
-    console.log('operation completed');
-    console.log('data received : ',data);
-  })
-  .catch((error)=>{
-    console.log('operation failed');
-    console.log('error : ',error);
-  })
-  .then(()=>{
-    console.log('I will execute no matter success or failure');
-    throw new Error('something unexpected happened');
-  })
-  .then(()=>{
-    console.log('I will execute too!');
-  })
-  .catch((errormsg)=>{1
-    console.log(errormsg)
-  })
+  editIndex=-1;
+  // listOfTransactions.push(transaction);
+  tnxNameHolder.value="";
+  tnxAmountHolder.value="";
+  render();
+  cancelButton.style.display=none;
 
+})
+
+loadData();
+render();
