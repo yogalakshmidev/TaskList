3af95ce5b1c51c383983c07441c9b11724ca0e9c
Yogalakshmi <yogarajakanibe@gmail.com>
2024-11-17 09:30:14 +0530

    form design updated

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 Expense Tracker design used to track the daily expenses.
-In this, we can also able to add, delete and edit the details basic design
+Include input fields for entering income and expense details (e.g., description, amount).
+Include 3 filters [ “all”, “income”, “expense” ] to get only the needed data. (use radio buttons)
+Display a list of all entries with options to edit or delete each one.
+Show the total income, total expenses, and net balance at the top.
+Create a reset button to clear the input fields
+
--- index.js
+++ index.js
@@ -1,111 +1,21 @@
-const currencyHolder=document.getElementById("currency");
-const balanceHolder=document.getElementById("balance");
+const netBalance = document.getElementById("btnTotal");
+console.log(netBalance.textContent);
+const incomeBalance = document.getElementById("btnIncome");
 
-const tnxNameHolder=document.getElementById("name");
-const tnxAmountHolder=document.getElementById("amount");
+const expenseBalance = document.getElementById("btnExpense");
 
-const income=document.getElementById("income");
-const expense=document.getElementById("expense");
+const descReason = document.getElementById("descReason");
+console.log("Description content", descReason.innerText);
+const amountSpend = document.getElementById("descAmount");
+console.log("descrioption amount", amountSpend.innerText);
+const optionAll = document.getElementById("allAmount");
+console.log("option selected using radiobutton", optionAll.id);
+const optionIncome = document.getElementById("incomeAmount");
 
-const saveButton=document.getElementById("save");
-const cancelButton=document.getElementById("cancel");
-const displayList=document.getElementById("list_of_transactions");
+const optionExpense = document.getElementById("expenseAmount");
 
-let symbol="$";
-let listOfTransactions = [];
-let currentBalance = 0;
-let editIndex = -1;
+const saveOption = document.getElementById("saveBtn");
+console.log("selected button", saveOption.id);
+const resetOption = document.getElementById("resetBtn");
 
-function edit(i){
-  editIndex = i;
-tnxNameHolder.value = listOfTransactions[i].name;
-tnxAmountHolder.value = listOfTransactions[i].amount;
-if(listOfTransactions[i].type == "income"){
-  income.checked = true;
-}
-else{
-  expense.checked = true;
-}
-cancelButton.style.display="block";
-}
-function del(i){
-  listOfTransactions = listOfTransactions.filter((e,index) => i !== index);
-  render();
-}
-
-function saveData(){
-  localStorage.setItem("symbol",symbol);
-  localStorage.setItem("balance",currentBalance);
-  localStorage.setItem("list",JSON.stringify(listOfTransactions));
-}
-function render(){
-   currentBalance =  listOfTransactions.reduce((total,value)=>{
-    return value.type == "expense" ? total - value.amount : total + value.amount
-  }, 0)
-
-    displayList.innerHTML="";
-
-    if(listOfTransactions.length == 0){
-      displayList.innerHTML +="No Transactions found"
-    }
-
-    else{
-      listOfTransactions.forEach((e,i) =>{
-        displayList.innerHTML +=` 
-        <li class="transaction ${e.type}">
-          <p>${e.name}</p>
-          <div class="right_side">
-            <p>${symbol}${e.amount}</p>
-            <button onclick="edit(${i})"><i class="fa-regular fa-pen-to-square"></i></button>
-            <button onclick="del(${i})"><i class="fa-solid fa-trash"></i></button>
-          </div>
-        </li>
-        `;
-      })
-    }
-
-
-  currencyHolder.innerHTML = symbol;
-  balanceHolder.innerHTML = currentBalance;
-  saveData();
-
-}
-
-cancelButton.addEventListener("click",()=>{
-  editIndex=-1;
-  // listOfTransactions.push(transaction);
-  tnxNameHolder.value="";
-  tnxAmountHolder.value="";
-  cancelButton.style.display="none";
-})
-
-saveButton.addEventListener("click",()=>{
-  if(tnxNameHolder.value == "" || Number(tnxAmountHolder.value) <=0){
-    console.log("Please enter proper transaction");
-    return;
-  }
-  let transaction = {
-    name: tnxNameHolder.value,
-    amount:Number(tnxAmountHolder.value),
-    type:income.checked? "income" :"expense"
-
-  };
-  console.log(transaction);
-if(editIndex == -1){ 
-  listOfTransactions.push(transaction);
-}
-else{
-  listOfTransactions[editIndex]=transaction;
-  }
-
-  editIndex=-1;
-  // listOfTransactions.push(transaction);
-  tnxNameHolder.value="";
-  tnxAmountHolder.value="";
-  render();
-  cancelButton.style.display="none";
-
-})
-
-loadData();
-render();
+const showOption = document.getElementById("showBtn");
